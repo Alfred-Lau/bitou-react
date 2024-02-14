bfd1f766a32bdff5626bb3c480909e8fb6128e9d
Alfred-Lau <jian.liu@icloud.com>
2024-02-14 15:58:39 +0800

    feat: 单节点 diff 改造

--- packages/react-reconciler/src/childFiber.ts
+++ packages/react-reconciler/src/childFiber.ts
@@ -26,77 +26,101 @@ function ChildReconciler(shouldTrackEffects: boolean) {
 		}
 	}
 
+	function deleteRemainingChildren(
+		returnFiber: FiberNode,
+		currentFirstChild: FiberNode | null
+	) {
+		if (!shouldTrackEffects) {
+			// 不追踪副作用
+			return;
+		}
+		// 追踪副作用
+		let childToDelete = currentFirstChild;
+		while (childToDelete !== null) {
+			deleteChild(returnFiber, childToDelete);
+			childToDelete = childToDelete.sibling;
+		}
+	}
+
+	// 单一元素
+	function reconcileSingleElement(
+		returnFiber: FiberNode,
+		currentFiber: FiberNode | null,
+		element: ReactElementType
+	) {
+		const key = element.key;
+		while (currentFiber !== null) {
+			// update
+			if (currentFiber.key === key) {
+				// key 相同
+				if (element.$$typeof === REACT_ELEMENT_TYPE) {
+					if (currentFiber.type === element.type) {
+						// type 相同
+						const existing = useFiber(currentFiber, element.props);
+						existing.return = returnFiber;
+						// 当前节点可复用，剩下节点要删除
+						deleteRemainingChildren(returnFiber, currentFiber.sibling);
+						return existing;
+					}
+					//key相同， type 不同,删除所有旧的
+					deleteRemainingChildren(returnFiber, currentFiber);
+					break;
+				} else {
+					if (__DEV__) {
+						console.warn('reconcilerChildFibers: 未知的 child 类型', element);
+						break;
+					}
+				}
+			} else {
+				// key 不同,删除
+				deleteChild(returnFiber, currentFiber);
+				currentFiber = currentFiber.sibling;
+			}
+		}
+		const fiber = createFiberFromElement(element);
+		fiber.return = returnFiber;
+		return fiber;
+	}
+
+	// 单一节点
+	function reconcileSingleTextNode(
+		returnFiber: FiberNode,
+		currentFiber: FiberNode | null,
+		content: string | number
+	) {
+		while (currentFiber !== null) {
+			// update flow
+			if (currentFiber.tag === HostText) {
+				// 类型没有变，可以复用
+				const existing = useFiber(currentFiber, { content });
+				existing.return = returnFiber;
+				deleteRemainingChildren(returnFiber, currentFiber.sibling);
+				return existing;
+			}
+			deleteChild(returnFiber, currentFiber);
+			currentFiber = currentFiber.sibling;
+		}
+		// 都不能复用，创建新的 fiber
+		const fiber = new FiberNode(HostText, { content }, null);
+		fiber.return = returnFiber;
+		return fiber;
+	}
+
+	function placeSingleChild(fiber: FiberNode) {
+		if (shouldTrackEffects && fiber.alternative === null) {
+			// 首屏渲染并且应该追踪副作用
+			fiber.flags |= Placement;
+		}
+
+		return fiber;
+	}
+
 	// 闭包的作用：1. 保存 shouldTrackEffects 的值；2. 保存 shouldTrackEffects 的值
 	return function reconcilerChildFibers(
 		returnFiber: FiberNode,
 		currentFiber: FiberNode | null,
 		newChild?: ReactElementType
 	) {
-		function reconcileSingleElement(
-			returnFiber: FiberNode,
-			currentFiber: FiberNode | null,
-			element: ReactElementType
-		) {
-			const key = element.key;
-			work: if (currentFiber !== null) {
-				// update
-				if (currentFiber.key === key) {
-					// key 相同
-					if (element.$$typeof === REACT_ELEMENT_TYPE) {
-						if (currentFiber.type === element.type) {
-							// type 相同
-							const existing = useFiber(currentFiber, element.props);
-							existing.return = returnFiber;
-							return existing;
-						}
-						//key相同， type 不同,删除
-						deleteChild(returnFiber, currentFiber);
-						break work;
-					} else {
-						if (__DEV__) {
-							console.warn('reconcilerChildFibers: 未知的 child 类型', element);
-							break work;
-						}
-					}
-				} else {
-					// key 不同,删除
-					deleteChild(returnFiber, currentFiber);
-				}
-			}
-			const fiber = createFiberFromElement(element);
-			fiber.return = returnFiber;
-			return fiber;
-		}
-
-		function reconcileSingleTextNode(
-			returnFiber: FiberNode,
-			currentFiber: FiberNode | null,
-			content: string | number
-		) {
-			if (currentFiber !== null) {
-				// update flow
-				if (currentFiber.tag === HostText) {
-					// 类型没有变，可以复用
-					const existing = useFiber(currentFiber, { content });
-					existing.return = returnFiber;
-					return existing;
-				}
-				deleteChild(returnFiber, currentFiber);
-			}
-			const fiber = new FiberNode(HostText, { content }, null);
-			fiber.return = returnFiber;
-			return fiber;
-		}
-
-		function placeSingleChild(fiber: FiberNode) {
-			if (shouldTrackEffects && fiber.alternative === null) {
-				// 首屏渲染并且应该追踪副作用
-				fiber.flags |= Placement;
-			}
-
-			return fiber;
-		}
-
 		// 单节点的情况
 		if (newChild !== null && typeof newChild === 'object') {
 			switch (newChild?.$$typeof) {
