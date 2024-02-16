c791355b5632741a941c556a9033611b04b611f7
Alfred-Lau <jian.liu@icloud.com>
2024-02-16 10:26:20 +0800

    feat: update 改造为队列

--- packages/react-reconciler/src/updateQueue.ts
+++ packages/react-reconciler/src/updateQueue.ts
@@ -4,6 +4,7 @@ import { Dispatch } from 'react/src/currentDispatcher';
 
 export interface Update<State> {
 	action: Action<State>;
+	next: Update<any> | null;
 }
 
 export interface UpdateQueue<State> {
@@ -15,7 +16,8 @@ export interface UpdateQueue<State> {
 
 export const createUpdate = <State>(action: Action<State>): Update<State> => {
 	return {
-		action
+		action,
+		next: null
 	};
 };
 
@@ -32,6 +34,17 @@ export const enqueueUpdate = <State>(
 	updateQueue: UpdateQueue<State>,
 	update: Update<State>
 ) => {
+	// 调整支持批处理逻辑
+	const pending = updateQueue.shared.pending;
+
+	// 每插入一个新的 update，都会将其 next 指向 pending.next，然后将 pending.next 指向 update，这样就形成了一个环状链表，并且 pending.next 指向的是最新的 update
+	if (pending === null) {
+		update.next = update;
+	} else {
+		update.next = pending.next;
+		pending.next = update;
+	}
+
 	updateQueue.shared.pending = update;
 };
 
