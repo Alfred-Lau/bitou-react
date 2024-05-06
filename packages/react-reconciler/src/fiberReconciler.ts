1312d250dea82c38a6aa03181a2b12e23e279706
Alfred-Lau <jian.liu@icloud.com>
2024-05-06 23:40:06 +0800

    feat: 首屏渲染默认使用同步更新

--- packages/react-reconciler/src/fiberReconciler.ts
+++ packages/react-reconciler/src/fiberReconciler.ts
@@ -10,6 +10,10 @@ import {
 import { ReactElementType } from 'shared/ReactTypes';
 import { scheduleUpdateOnFiber } from './workLoop';
 import { requestUpdateLane } from './fiberLanes';
+import {
+	unstable_ImmediatePriority,
+	unstable_runWithPriority
+} from 'scheduler';
 
 export function createContainer(container: Container) {
 	const hostRootFiber = new FiberNode(HostRoot, {}, null);
@@ -22,15 +26,19 @@ export function updateContainer(
 	element: ReactElementType | null,
 	root: FiberRootNode
 ) {
-	const hostRootFiber = root.current;
-	const lane = requestUpdateLane();
-	const update = createUpdate<ReactElementType | null>(element, lane);
-	// 接入更新机制
-	enqueueUpdate(
-		hostRootFiber.updateQueue as UpdateQueue<ReactElementType | null>,
-		update
-	);
-	// 调度更新
-	scheduleUpdateOnFiber(hostRootFiber, lane);
+	// 首屏渲染同步执行
+	unstable_runWithPriority(unstable_ImmediatePriority, () => {
+		const hostRootFiber = root.current;
+		const lane = requestUpdateLane();
+		const update = createUpdate<ReactElementType | null>(element, lane);
+		// 接入更新机制
+		enqueueUpdate(
+			hostRootFiber.updateQueue as UpdateQueue<ReactElementType | null>,
+			update
+		);
+		// 调度更新
+		scheduleUpdateOnFiber(hostRootFiber, lane);
+	});
+
 	return element;
 }
